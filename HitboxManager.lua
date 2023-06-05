fdc37dbf4e2bf09dcbda221cda2574b23ca09d28
EricPokemon <115324489+EricPokemon@users.noreply.github.com>
2023-06-05 13:49:14 -0400

    Update HitboxManager.lua

--- HitboxManager.lua
+++ HitboxManager.lua
@@ -1,7 +1,7 @@
 --[[
-    Silly Cats Manages Hitboxes v2.1
+    Silly Cats Manages Hitboxes v2.2
     An open source magnitude hitbox module created by Eric_pokemon.
-    Last edited: 04/15/23
+    Last edited: 05/17/23
     
     More information and documentation can be found here: 
     https://devforum.roblox.com/t/2013747
@@ -13,6 +13,11 @@
     v1.0 and v1.1 scripts will not work. Future versions will be compatible with v2.
 ]]
 
+-- localalized because apperently that make stuff go fast
+local clock = os.clock
+local tableFind, tableInsert, tableRemove = table.find, table.insert, table.remove
+local dead = Enum.HumanoidStateType.Dead
+local instanceNew, vectorOne, brickColor, ball = Instance.new, Vector3.one, BrickColor.new, Enum.PartType.Ball
 
 export type SillyHitbox = {
 	Part: BasePart,
@@ -20,7 +25,6 @@ export type SillyHitbox = {
 	EndTime: number,
 	IsImmune: {Model?},
 	Hit: RBXScriptSignal,
-	_Janitor: never,
 
 	Visual: Part?
 }
@@ -35,7 +39,10 @@ local Heartbeat = game:GetService("RunService").Heartbeat
 
 -- Settings
 local AutoAddPlayers: boolean? = true
-local VisualHitboxes: boolean? = true
+local VisualHitboxes: boolean? = false
+local RangeCheck: number = 5 --[[ basically multiplies with the hitbox range and duration. 
+And only checks that area of magntiude. Increase if your game has a lot of movement. I recommend keeping it at 5 
+if you don't really have that much movement.]]
 
 -- Variables
 local CleanUpLoop = Janitor.new()
@@ -55,17 +62,17 @@ local function VisualizeHitbox(Hitbox: SillyHitbox)
     ]]
 
 	if VisualHitboxes then
-		local NewVisual = Instance.new("Part")
+		local NewVisual = instanceNew("Part")
 		NewVisual.Parent = workspace
 		NewVisual.CFrame = Hitbox.Part.CFrame
-		NewVisual.Size = Vector3.new(Hitbox.Range*2,Hitbox.Range*2,Hitbox.Range*2)
+		NewVisual.Size = vectorOne * Hitbox.Range * 2
 		NewVisual.CanCollide = false
 		NewVisual.Massless = true
-		NewVisual.BrickColor = BrickColor.new("Bright red")
+		NewVisual.BrickColor = brickColor("Bright red")
 		NewVisual.Transparency = .7
-		NewVisual.Shape = Enum.PartType.Ball
+		NewVisual.Shape = ball
 
-		local weld = Instance.new("WeldConstraint")
+		local weld = instanceNew("WeldConstraint")
 		weld.Parent = NewVisual
 		weld.Part0 = NewVisual
 		weld.Part1 = Hitbox.Part
@@ -74,38 +81,7 @@ local function VisualizeHitbox(Hitbox: SillyHitbox)
 	end
 end
 
-
-
-local function isCharacterDead(Character: Model)
-	return not Character
-		or not Character.Parent
-		or Character.Humanoid:GetState() == Enum.HumanoidStateType.Dead
-		or not Character.Humanoid 
-		or Character.Humanoid.Health <= 0
-end
-
-
-
-local function CheckHumanoids(Hitbox: SillyHitbox)
-    --[[
-        Description: Check if any characters are in the hitbox. They will not be hit again unless the table hitbox.hit got rid of 
-        the character. Some more information could be found in the 'HitboxChecker' function.
-        Arguments: Hitbox (custom dictionary type.)
-        Returns: Void
-    ]]
-
-	for _,Character in ipairs(Characters) do
-		-- will ignore this character if the character has already been hit, not in range or is dead 
-		if isCharacterDead(Character) or (Hitbox.Part.Position - Character.HumanoidRootPart.Position).Magnitude > Hitbox.Range or table.find(Hitbox.IsImmune, Character) then
-			continue
-		end
-
-		table.insert(Hitbox.IsImmune,Character)
-		Hitbox.Hit:Fire(Character.Humanoid)
-	end
-end
-
-
+local CurrentClock
 
 local function HitboxChecker()
     --[[
@@ -121,12 +97,31 @@ local function HitboxChecker()
 		return
 	end
 
-	for _,Hitbox in ipairs(Managing) do
-		if os.clock() >= Hitbox.EndTime or not Hitbox.Part then 
+	CurrentClock = clock()
+
+	for _,Hitbox in Managing do
+		if CurrentClock >= Hitbox.EndTime or not Hitbox.Part or (#Hitbox.Check == 0 and not Hitbox.Tick) then 
 			HitboxManager.Destroy(Hitbox)
 			continue
 		end
-		CheckHumanoids(Hitbox)
+
+		if Hitbox.Tick and CurrentClock >= Hitbox.Tick then
+			Hitbox.Tick = CurrentClock + Hitbox.ChangeTick
+
+			for _, Character in Hitbox.OldCheck do
+				tableInsert(Hitbox.Check, Character)
+			end
+		end
+
+		for Number, Character in Hitbox.Check do
+			-- will ignore this character if the character has already been hit, not in range or is dead 
+			if (Hitbox.Part.Position - Character.HumanoidRootPart.Position).Magnitude > Hitbox.Range then
+				continue
+			end
+
+			tableRemove(Hitbox.Check, Number)
+			Hitbox.Hit:Fire(Character.Humanoid)
+		end
 	end
 end
 
@@ -140,12 +135,12 @@ function HitboxManager.AddCharacter(Character: Model): ()
         Returns: Void
     ]]
 
-	if table.find(Characters,Character) then warn("Character has already been added. Won't be readding.") return end
+	if tableFind(Characters,Character) then warn("Character has already been added. Won't be readding.") return end
 
 	local Humanoid = Character:FindFirstChild("Humanoid") or warn("Character won't be detected to hitboxes because 'Humanoid' doesn't exist.")
 	local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart") or warn("Character won't be detected to hitboxes because 'HumanoidRootPart' doesn't exist.") 
 
-	table.insert(Characters,Character)
+	tableInsert(Characters,Character)
 
 	-- Signals
 	local DieSignal
@@ -157,7 +152,7 @@ function HitboxManager.AddCharacter(Character: Model): ()
 		DestroyingSignal:Disconnect()
 		DestroyingSignal = nil
 
-		table.remove(Characters,table.find(Characters,Character))
+		tableRemove(Characters,tableFind(Characters,Character))
 	end)
 
 	DestroyingSignal = Character.Destroying:Once(function()
@@ -166,7 +161,7 @@ function HitboxManager.AddCharacter(Character: Model): ()
 		DestroyingSignal:Disconnect()
 		DestroyingSignal = nil
 
-		table.remove(Characters,table.find(Characters,Character))
+		tableRemove(Characters,tableFind(Characters,Character))
 	end)
 end
 
@@ -186,15 +181,28 @@ function HitboxManager.new(Part: BasePart, Range: number, Duration: number, Immu
 	local NewHitbox: SillyHitbox = {
 		Part = Part,
 		Range = Range,
-		EndTime = os.clock() + Duration,
-		IsImmune = Immune,
+		EndTime = clock() + Duration,
 		Hit = GoodSignal.new(),
-		_Janitor = Janitor.new()
+
+		-- stuff that won't be viewed by the user
+		OldCheck = {},
+		Check = {},
+		_Janitor = Janitor.new(),
 	}
 
-	table.insert(Managing,NewHitbox)
+	tableInsert(Managing,NewHitbox)
 	NewHitbox._Janitor:Add(NewHitbox.Hit,"DisconnectAll")
 	NewHitbox._Janitor:LinkToInstance(Part)
+
+	for _,Character in Characters do -- makes the hitbox only checks a certain range, saving resources.
+
+		if (NewHitbox.Part.Position - Character.HumanoidRootPart.Position).Magnitude > NewHitbox.Range * (Duration + RangeCheck) or tableFind(Immune, Character) then
+			continue
+		end
+
+		tableInsert(NewHitbox.OldCheck, Character)
+		tableInsert(NewHitbox.Check, Character)
+	end
 
 	if not CheckConnection then
 		CheckConnection = CleanUpLoop:Add(Heartbeat:Connect(HitboxChecker),"Disconnect")
@@ -205,7 +213,17 @@ function HitboxManager.new(Part: BasePart, Range: number, Duration: number, Immu
 	return NewHitbox
 end
 
+function HitboxManager.CreateTick(Hitbox: SillyHitbox, TickTime: number): ()
+	--[[
+		Description: This function will make a hitbox a tick attack for something like a barrage. 
+		TickTime means how often a hitbox will reset the projection in seconds.
+		Arguments: Hitbox (custom dictionary type.), TickTime(number)
+		Returns: Void
+	]]
 
+	Hitbox.Tick = clock() + TickTime 
+	Hitbox.ChangeTick = TickTime
+end
 
 function HitboxManager.Destroy(Hitbox: SillyHitbox): ()
     --[[
@@ -215,7 +233,7 @@ function HitboxManager.Destroy(Hitbox: SillyHitbox): ()
         Returns: Void
     ]]
 
-	local FindHitbox = table.find(Managing,Hitbox)
+	local FindHitbox = tableFind(Managing,Hitbox)
 
 	if FindHitbox then
 		Hitbox.Hit:DisconnectAll()
@@ -229,7 +247,7 @@ function HitboxManager.Destroy(Hitbox: SillyHitbox): ()
 			hasJanitor:Destroy()
 		end
 
-		table.remove(Managing,FindHitbox)
+		tableRemove(Managing,FindHitbox)
 
 		Hitbox = nil
 		FindHitbox = nil
@@ -240,7 +258,7 @@ end
 
 -- Runs settings
 if AutoAddPlayers then
-	for _, Player in ipairs(Players:GetPlayers()) do -- players whom loaded before keep this
+	for _, Player in Players:GetPlayers() do -- players whom loaded before keep this
 		HitboxManager.AddCharacter(Player.Character)
 	end
 
